38c606ee865148ed7a6154cb5ef24a8ac37e79c0
Jinseo Park <jinseopark94@gmail.com>
2021-03-16 01:13:00 +0900

    init

--- app/src/main/java/com/example/nm1/src/main/home/nest/rule/RuleFragment.kt
+++ app/src/main/java/com/example/nm1/src/main/home/nest/rule/RuleFragment.kt
@@ -1,6 +1,11 @@
 package com.example.nm1.src.main.home.nest.rule
 
 import androidx.fragment.app.Fragment
+import com.example.nm1.R
+import com.example.nm1.config.BaseFragment
+import com.example.nm1.databinding.FragmentRuleBinding
 
-class RuleFragment: Fragment() {
+class RuleFragment: BaseFragment<FragmentRuleBinding>(FragmentRuleBinding::bind, R.layout.fragment_rule){
+
+
 }
