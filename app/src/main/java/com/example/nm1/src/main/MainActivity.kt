d0b62cd47976c89d4624f3b06dbaa77e8e4806e0
yurrrri <leeyuri.andapp@gmail.com>
2021-03-16 09:33:22 +0900

    Revert "Merge branch 'main' into feature/todo"
    This reverts commit a7eb67e657e028c3f02ac3da91b26150d70f6413, reversing
    changes made to 095f5ce94067327a2f6a5ef42dec237aae9a7db5.

--- app/src/main/java/com/example/nm1/src/main/MainActivity.kt
+++ app/src/main/java/com/example/nm1/src/main/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         initNavigationBar()
     }
 
