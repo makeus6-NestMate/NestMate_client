4fd03375663992b5ed3883fcebd979a4441d7c56
yurrrri <leeyuri.andapp@gmail.com>
2021-03-16 10:06:34 +0900

    commit

--- app/src/main/java/com/example/nm1/src/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/nm1/src/main/home/HomeFragment.kt
@@ -1,21 +1,9 @@
 package com.example.nm1.src.main.home
 
-import android.os.Build
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nm1.R
-import kotlinx.android.synthetic.main.home_fragment.*
+import com.example.nm1.config.BaseFragment
+import com.example.nm1.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
-    }
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
+
 }
